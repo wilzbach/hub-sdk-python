59e283247285c044863090b34f84feb69d671c7e
Aditya Bansal <adi.bansal241996@gmail.com>
2019-08-16 15:19:11 +0530

    feat(Argument): start tracking argument is required or not

--- storyhub/sdk/service/Argument.py
+++ storyhub/sdk/service/Argument.py
@@ -6,12 +6,13 @@ class Argument(ServiceObject):
     Represents an argument, for an event or other service object.
     """
 
-    def __init__(self, name, help_, type_, data):
+    def __init__(self, name, help_, type_, required_, data):
         super().__init__(data=data)
 
         self._name = name
         self._help_ = help_
         self._type = type_
+        self._required = required_
 
     @classmethod
     def from_dict(cls, data):
@@ -24,6 +25,7 @@ class Argument(ServiceObject):
                 'help', '.not.available'
             ),
             type_=argument['type'],
+            required_=argument.get('required', False),
             data=data
         )
 
@@ -35,3 +37,6 @@ class Argument(ServiceObject):
 
     def type(self):
         return self._type
+
+    def required(self):
+        return self._required
