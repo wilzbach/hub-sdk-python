6d715544f6a739d69b2a3b06a331051769839b67
Sebastian Wilzbach <seb@wilzba.ch>
2019-10-15 15:23:52 +0200

    feat(version): version bump to 0.1.9

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ readme_contents = 'A Python SDK to access the Storyscript Hub, '
 
 setup(
     name='story-hub',
-    version='0.1.8',
+    version='0.1.9',
     description='A Python SDK to access the Storyscript Hub, '
                 'which supports caching and more',
     long_description=readme_contents,
